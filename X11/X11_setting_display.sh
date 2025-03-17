a1e33e62948158ab06a8c01229dee87844bfd74c
Kambiz Moradi <kambizmoradi2009@gmail.com>
2025-03-17 11:53:49 +0330

    X11 add display settings

---
+++ X11/X11_setting_display.sh
@@ -0,0 +1,4 @@
+#/usr/bin/sh
+xrandr --output eDP-1 --off --output DP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
+
+xrandr --output DP-1 --brightness 0.99
