47ff747c29479f851640afca4afc978418796140
Kambiz Moradi <kambizmoradi2009@gmail.com>
2025-08-22 08:45:23 +0330

    bin config_mouse.sh disable tuchpad

--- bin/dot-bin/config_mouse.sh
+++ bin/dot-bin/config_mouse.sh
@@ -7,8 +7,10 @@ xinput list | sed -ne 's/^[^ ][^V].*id=\([0-9]*\).*/\1/p' | while read id; do
   case $(xinput list-props $id) in
   *"libinput Middle Emulation Enabled Default"*)
     xinput set-prop $id "libinput Accel Speed" -1
+    xinput --set-prop $id 'Coordinate Transformation Matrix' 1 0 0, 0 1 0, 0 0 1
     echo "id=$id"
     # etc.
     ;;
   esac
 done
+xinput disable "Synaptics TM3053-004"
