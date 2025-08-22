e12a896496b3e8c670bd4cba83aaf6da3224a505
Kambiz Moradi <kambizmoradi2009@gmail.com>
2025-08-22 08:45:46 +0330

    bin config_slowmouse.sh

---
+++ bin/dot-bin/config_slowsmouse.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+# Disable Touchpad in labtops
+#xinput set-prop 'ETPS/2 Elantech Touchpad' 'Device Enabled' 0
+
+# Reduce mouse speed
+xinput list | sed -ne 's/^[^ ][^V].*id=\([0-9]*\).*/\1/p' | while read id; do
+  case $(xinput list-props $id) in
+  *"libinput Middle Emulation Enabled Default"*)
+    xinput set-prop $id "libinput Accel Speed" -1
+    xinput --set-prop $id 'Coordinate Transformation Matrix' 0.03 0 0, 0 0.03 0, 0 0 1
+    echo "id=$id"
+    # etc.
+    ;;
+  esac
+done
